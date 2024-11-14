223988a91b3b0c6952daa76fe9e0932570e37f7d
baiysh narkeev <narkeev123@gmail.com>
2024-11-13 16:57:31 -0800

    Add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image classifier using transfer learning',
+    author='Baiysh',
+    license='',
+)
